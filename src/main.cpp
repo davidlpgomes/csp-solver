9a016a0ae043faa53ea4893b5485b1c40e0e040b
David Gomes <david.gomes@tuta.io>
2024-05-24 11:38:09 -0300

    Update main

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,25 @@
 #include <iostream>
 
+#include "backtracking.hpp"
+#include "csp.hpp"
+
+using namespace csp;
+
 int main() {
+    Csp *csp{Csp::fromInput()};
+
+    #ifdef DEBUG
+    std::cout << "INFO: Running in DEBUG mode." << std::endl;
+
+    std::cout << "\n---------- PROBLEMA:" << std::endl;
+    csp->print();
+    std::cout << "---------------------" << std::endl;
+    #endif
+
+    // Backtracking
+    Backtracking::runBacktracking(csp);
+
+    delete csp;
 
     return 0;
 }
