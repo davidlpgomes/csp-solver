9abb04bf7bd05610aabf0276ea60e492138d0e03
David Gomes <david.gomes@tuta.io>
2024-05-24 11:37:23 -0300

    Add Restriction class

---
+++ src/restriction.cpp
@@ -0,0 +1,37 @@
+#include "restriction.hpp"
+
+#include <iostream>
+
+using namespace csp;
+
+Restriction::Restriction() {}
+
+Restriction* Restriction::fromInput() {
+    Restriction *r{new Restriction};
+
+    std::cin >> r->type;
+    r->type = r->type == 'V' ? 1 : 0;
+
+    std::cin >> r->scopeSize;
+
+    unsigned v;
+    for (unsigned i = 0; i < r->scopeSize; i++) {
+        std::cin >> v;
+        r->scope.push_back(v);
+    }
+
+    std::cin >> r->tupleQty;
+
+    for (unsigned i = 0; i < r->tupleQty; i++) {
+        std::vector<int> tuple;
+
+        for (unsigned j = 0; j < r->scopeSize; j++) {
+            std::cin >> v;
+            tuple.push_back(v); 
+        }
+
+        r->tuples.push_back(tuple);
+    }
+
+    return r;
+}
---
+++ src/restriction.hpp
@@ -0,0 +1,31 @@
+#ifndef RESTRICTION_HPP
+#define RESTRICTION_HPP
+
+#include <vector>
+
+
+namespace csp {
+
+class Restriction {
+    public:
+        Restriction();
+
+        virtual ~Restriction() = default;
+
+        static Restriction* fromInput();
+
+        // type: 0 -> invalid values, 1 -> valid values
+        char type;
+
+        unsigned scopeSize;
+        unsigned tupleQty;
+
+        std::vector<unsigned> scope;
+
+        // Uses vector due to possible scope increase
+        std::vector<std::vector<int>> tuples;
+};
+
+} // namespace csp
+
+#endif
